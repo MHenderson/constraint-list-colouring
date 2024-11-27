a3bb8224e8044f8f77184d272f7f258b9dcca9e6
Matthew Henderson <matthew.james.henderson@gmail.com>
2024-11-27 10:35:21 +0000

    Improve visualisations.

--- src/petersen-edge.py
+++ src/petersen-edge.py
@@ -14,18 +14,18 @@ G_layout = nx.shell_layout(G, nlist = [range(5, 10), range(5)], rotate = 0.)
 edge_colours = nx.get_edge_attributes(G,'colour').values()
 
 nx.draw(G,
-                pos = G_layout,
-         edge_color = edge_colours,
+        pos = G_layout,
+        edge_color = edge_colours,
         with_labels = False,
-          edge_cmap = plt.cm.tab10,
-              width = 5,
-          node_size = 150,
-         node_color = 'black')
+        edge_cmap = plt.cm.tab10,
+        width = 5,
+        node_size = 150,
+        node_color = 'black')
 
-edge_labels = dict([((n1, n2), str(d['colour']) + ': ' + str(sorted(d['permissible']))) for n1, n2, d in G.edges(data = True)])
+edge_labels = dict([((n1, n2), str(sorted(d['permissible']))) for n1, n2, d in G.edges(data = True)])
 X = nx.draw_networkx_edge_labels(G, pos = G_layout, edge_labels = edge_labels, font_size = 6)
 
-patches = [mpatches.Patch(color = plt.cm.tab10(i), label = f'{i}') for i in edge_colours]
-plt.legend(handles = patches, title = "Colours", bbox_to_anchor = (1, 1))
+patches = [mpatches.Patch(color = plt.cm.tab10(i), label = f'{i}') for i in range(10)]
+plt.legend(handles = patches, bbox_to_anchor = (1.1, 1))
 
 plt.savefig("png/petersen-edge.png", format = "PNG")
--- src/petersen-node.py
+++ src/petersen-node.py
@@ -5,30 +5,28 @@ import matplotlib.patches as mpatches
 import vizing as vz
 
 G = nx.petersen_graph()
-G = lc.list_init_node(G, range(1, 10), 3, 0)
+G = lc.list_init_node(G, range(0, 10), 3, 0)
 G = vz.node_list_colouring_solution(G)
 
 G_layout = nx.shell_layout(G, nlist = [range(5, 10), range(5)], rotate = 0.)
 
-node_labels = dict([(n, str(d['colour']) + ': ' + str(d['permissible'])) for n, d in G.nodes(data = True)])
+node_labels = dict([(n, str(sorted(d['permissible']))) for n, d in G.nodes(data = True)])
 
 node_colors = set(nx.get_node_attributes(G, 'colour').values())
 
-patches = [mpatches.Patch(color = plt.cm.tab10(i), label = f'{i}') for i in node_colors]
-
 nx.draw(G,
         G_layout,
-        edge_color = "black",
-        node_color = list(nx.get_node_attributes(G, 'colour').values()),
-        node_shape = 's',
-        width = 10,
-        node_size = 1300,
-        cmap = plt.cm.tab10,
-        labels = node_labels,
-        font_size = 6,
+         edge_color = "black",
+         node_color = list(nx.get_node_attributes(G, 'colour').values()),
+              width = 4,
+          node_size = 1000,
+               cmap = plt.cm.tab10,
+             labels = node_labels,
+          font_size = 6,
         with_labels = True
 )
 
-plt.legend(handles = patches, title = "Colours", bbox_to_anchor = (1, 1))
+patches = [mpatches.Patch(color = plt.cm.tab10(i), label = f'{i}') for i in range(1, 10)]
+plt.legend(handles = patches, bbox_to_anchor = (1.1, 1))
 
 plt.savefig("png/petersen-node.png", format = "PNG")
